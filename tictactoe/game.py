2b17971fd3a50b96996cb120dcfb5b557164dc01
Mario Corchero <mariocj89@gmail.com>
2016-07-04 17:26:26 +0100

    Python 3 complient

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -3,26 +3,49 @@ import random
 import itertools
 from tictactoe import board
 
+try:
+    input = raw_input
+except NameError:
+    pass
 
-class HumanPlayer(object):
+
+class Player(object):
+
+    TITLE = 'N/A'
+
+    def __init__(self, role, in_board):
+        """Initializes the player with a handle the the board where the game is played"""
+        self.role = role
+        self.board = in_board
+        assert role in [board.CIRCLE, board.CROSS]
+
+    def play(self):
+        row, column = self._next_movement()
+        if self.role == board.CROSS:
+            self.board.x(row, column)
+        else:
+            self.board.o(row, column)
+        print("Bot plays {}, {}".format(row, column))
+
+    def _next_movement(self):
+        """Returns the a tuple that represents the desired movement of the player"""
+        raise NotImplementedError()
+
+    def __str__(self):
+        return "{} {}".format(self.role, self.TITLE)
+
+
+class HumanPlayer(Player):
     """Represents a player that interacts through stdin"""
 
     TITLE = "Human"
 
-    def __init__(self, in_board):
-        """Initializes the player with a handle the the board where the game is played
-
-        The player should not modify the board directly
-        """
-        self.board = in_board
-
-    def play(self):
-        """Returns the desired movement of the player"""
+    def _next_movement(self):
         print(self.board)
         while True:
             try:
-                row = int(raw_input("row: "))
-                column = int(raw_input("column: "))
+                row = int(input("row: "))
+                column = int(input("column: "))
             except ValueError:
                 print("Invalid number")
                 continue
@@ -35,16 +58,12 @@ class HumanPlayer(object):
             return row, column
 
 
-class RandomComputerPlayer(object):
+class RandomComputerPlayer(Player):
     """IA player that plays randomly"""
 
     TITLE = "Random IA"
 
-    def __init__(self, in_board):
-        self.board = in_board
-
-    def play(self):
-        """Returns the desired movement of the player"""
+    def _next_movement(self):
         while True:
             row = random.randint(0, 2)
             column = random.randint(0, 2)
@@ -57,38 +76,32 @@ class Game(object):
     """Class to handle all interaction between players and the board"""
     def __init__(self):
         self.board = board.Board()
-        options = [board.CROSS, board.CIRCLE]
-        random.shuffle(options)
-        self.turns = itertools.cycle(options)
 
-    def create_player(self):
+    def create_player(self, role):
         options = {
             "H": HumanPlayer,
             "R": RandomComputerPlayer,
         }
         while True:
-            print("Choose a Player: ")
+            print("Choose a Player for {}: ".format(role))
             for k, v in options.items():
                 print("{}) {}".format(k, v.TITLE))
-            option = raw_input("Type Initial: ")[0]
+            option = input("Type Initial: ")[0]
             if option[0] in options:
                 res = options[option]
                 print("You have chosen: {}".format(res.TITLE))
-                return res(self.board)
+                return res(role, self.board)
 
     def play(self):
-        cross_player = self.create_player()
-        circle_player = self.create_player()
+        players = [self.create_player(board.CROSS),
+                   self.create_player(board.CIRCLE)]
+        random.shuffle(players)
+        players = itertools.cycle(players)
         while True:
-            turn = next(self.turns)
+            player = next(players)
 
-            print("Turn of: '{}'".format(turn))
-            if turn == board.CROSS:
-                row, column = cross_player.play()
-                self.board.x(row, column)
-            elif turn == board.CIRCLE:
-                row, column = circle_player.play()
-                self.board.o(row, column)
+            print("Turn of: '{}'".format(player))
+            player.play()
 
             if self.board.winner:
                 print("{} wins!".format(self.board.winner))
